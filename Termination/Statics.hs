1a1c6aa11f7987f347b55dc236020e4598c0e771
Max Bolingbroke <batterseapower@hotmail.com>
2010-10-26 00:30:20 +0100

    Resolve a FIXME: make input static variables eligible for generalisation

--- Termination/Statics.hs
+++ Termination/Statics.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Termination.Statics (
     embedStatics
   ) where
@@ -9,19 +10,35 @@ import Supercompile.Split (StaticSort(..), Statics(..))
 
 import Core.Renaming (Out)
 import Core.Syntax (Var)
-import Evaluator.Syntax (annedTag)
+import Evaluator.Syntax (annee, annedTag)
 
 import Utilities
 
 import qualified Data.IntMap as IM
 import qualified Data.Map as M
+import qualified Data.Foldable as Foldable
+import qualified Data.Traversable as Traversable
+
+
+-- | A wrapper for variables that were free in the initial program (of which there are guaranteed to be a finite number)
+newtype InputVar = UnsafeCertifyInputVar (Out Var)
+                 deriving (Eq, Ord)
+instance Finite InputVar
 
 
 embedStatics :: WQO Statics StaticGeneraliser
-embedStatics = precomp (staticsTags . staticVars) $ postcomp generaliserFromGrowing $ refineCollection (\discard -> postcomp discard $ zippable nat)
+embedStatics = precomp (staticsTags . staticVars) $ postcomp generaliserFromGrowing (bag `prod` bag)
   where
-    staticsTags :: M.Map (Out Var) StaticSort -> TagMap Nat
-    staticsTags fvs = IM.unionsWith (+) [IM.singleton tg 1 | LocalVariable tg <- M.elems fvs] -- FIXME: make InputVariable eligible for generalisation
+    bag :: (HasDomain f, Finite (Domain f),                 -- Only works for things with finite domains
+            Foldable.Foldable f, Traversable.Traversable f) -- We insist that the things are Traversable (and hence Functor) so we can actually do some structure-preserving operations on the versions with domain equality evidence
+        => WQO (f Nat) (f Bool)
+    bag = refineCollection (\discard -> postcomp discard $ zippable nat)
     
-    generaliserFromGrowing :: TagMap Bool -> StaticGeneraliser
-    generaliserFromGrowing growing = StaticGeneraliser $ \x -> fromMaybe False $ IM.lookup (annedTag x) growing
+    staticsTags :: M.Map (Out Var) StaticSort -> (TagMap Nat, M.Map InputVar Nat)
+    staticsTags statics = (IM.unionsWith (+) [IM.singleton tg 1 | LocalVariable tg <- M.elems statics],
+                           M.unionsWith (+) [M.singleton (UnsafeCertifyInputVar x) 1 | (x, InputVariable) <- M.toList statics])
+    
+    generaliserFromGrowing :: (TagMap Bool, M.Map InputVar Bool) -> StaticGeneraliser
+    generaliserFromGrowing (growing_locals, growing_inputs)
+      = StaticGeneraliser $ \x -> fromMaybe False $ IM.lookup (annedTag x) growing_locals `mplus`
+                                                    M.lookup (UnsafeCertifyInputVar (annee x)) growing_inputs
