6ca96e80242e99b3b15eaedf18bdcd06c7abc090
Max Bolingbroke <batterseapower@hotmail.com>
2010-10-25 17:15:57 +0100

    Simplify the sc loop now that we don't forget the future history and use future history data

--- Supercompile/Drive.hs
+++ Supercompile/Drive.hs
@@ -233,16 +233,15 @@ catchScpM :: ((forall b. c -> ScpM b) -> ScpM a) -- ^ Action to try: supplies a 
 catchScpM f_try f_abort = ScpM $ \e s k -> unScpM (f_try (\c -> ScpM $ \_ _ _ -> unScpM (f_abort c) e s k)) e s k
 
 
-type SCHistory = History (State, Maybe SCRollback) (Generaliser, Maybe SCRollback)
-newtype SCRollback = SCRB { rollbackWith :: (Generaliser, SCHistory) -> ScpM (Deeds, Out FVedTerm) }
+newtype SCRollback = SCRB { rollbackWith :: Generaliser -> ScpM (Deeds, Out FVedTerm) }
 
-sc, sc' :: SCHistory -> (Deeds, Statics, State) -> ScpM (Deeds, Out FVedTerm)
+sc, sc' :: History (State, SCRollback) (Generaliser, SCRollback) -> (Deeds, Statics, State) -> ScpM (Deeds, Out FVedTerm)
 sc  hist = memo (sc' hist)
-sc' hist (deeds, statics, state) = (\raise -> check (Just (SCRB raise))) `catchScpM` \(gen, _hist') -> stop gen hist -- TODO: I want to use the original history here, but I think doing so leads to non-term as it contains rollbacks from "below us" (try DigitsOfE2)
+sc' hist (deeds, statics, state) = (\raise -> check (SCRB raise)) `catchScpM` \gen -> stop gen hist -- TODO: I want to use the original history here, but I think doing so leads to non-term as it contains rollbacks from "below us" (try DigitsOfE2)
   where
     check mb_rb = case terminate hist (state, mb_rb) of
-                    Continue hist'    -> continue hist'
-                    Stop (gen, mb_rb) -> maybe (stop gen hist) (`rollbackWith` (gen, hist)) $ guard sC_ROLLBACK >> mb_rb
+                    Continue hist' -> continue hist'
+                    Stop (gen, rb) -> maybe (stop gen hist) (`rollbackWith` gen) $ guard sC_ROLLBACK >> Just rb
     stop gen hist = trace "sc-stop" $ split gen               (sc hist) (deeds, statics, state)
     continue hist =                   split generaliseNothing (sc hist) ((\res@(_, _, state') -> traceRender ("reduce end", residualiseState state') res) $
                                                                          case reduce (deeds, state) of (deeds, state) -> (deeds, statics, state)) -- TODO: experiment with doing admissability-generalisation on reduced terms. My suspicion is that it won't help, though (such terms are already stuck or non-stuck but loopy: throwing stuff away does not necessarily remove loopiness).
