7d1de8cd667cef07b21c0e5896101f072e98eff1
Max Bolingbroke <batterseapower@hotmail.com>
2010-10-28 20:08:55 +0100

    Improvement to the speculation engine: do not allow terms (even winning ones) to force members of the heap that have proved to be losers in the past
    Conflicts:
    
    	Evaluator/Evaluate.hs

--- Evaluator/Evaluate.hs
+++ Evaluator/Evaluate.hs
@@ -100,25 +100,35 @@ step reduce live (losers, deeds, (h, k, (rn, Comp (Tagged tg (FVed _ e))))) = ca
         linear = annee x' `S.notMember` pureHeapFreeVars h (stackFreeVars k (inFreeVars annedValueFreeVars' (rn, v))) &&
                  annee x' `S.notMember` live
 
-    allocate :: Deeds -> Heap -> Stack -> In ([(Var, AnnedTerm)], AnnedTerm) -> (Losers, Deeds, State)
+    allocate :: Deeds -> Heap -> Stack -> In ([(Var, AnnedTerm)], AnnedTerm) -> (Losers, Deeds, State) -- FIXME: I suspect we should accumulate Losers across the boundary of sc
     allocate deeds (Heap h ids) k (rn, (xes, e)) = (losers', deeds', (heap', k, (rn', e)))
       where
         (ids', rn', xes') = renameBounds (\_ x' -> x') ids rn xes
         (losers', deeds', heap')
           | not sPECULATION = (losers, deeds, Heap (h `M.union` M.fromList xes') ids')
-          | otherwise = foldl' speculate_one (losers, deeds, Heap h ids') (xes' `zip` lives')
+          | otherwise       = (final_losers, final_deeds, Heap (final_h_losers `M.union` final_h_winners) final_ids)
             where
+              -- It is *very important* that we prevent speculation from forcing heap-carried things that have proved
+              -- to be losers in the past. This prevents us discovering speculation failure of some head binding, and
+              -- then forcing several thunks that are each strict in it. This change was motivated by DigitsOfE2 -- it
+              -- speeds up supercompilation of that benchmark massively.
+              (h_losers, h_winners) = M.partition (\in_e -> annedTag (snd in_e) `IS.member` losers) h
+              
+              (final_losers, final_deeds, _final_live, final_h_losers, Heap final_h_winners final_ids) = foldl' speculate_one (losers, deeds, S.empty, h_losers, Heap h_winners ids') (xes' `zip` lives')
+                
               -- Construct the live set for use when speculating each heap binding. This prevent us from accidentally GCing something that is live
               -- in the "continuation" when speculating a heap binding. We ensure that only those heap bindings that occur *strictly later* than
               -- the binding being speculated contribute to the live set -- this means that GC can still collect stuff that speculation truly makes dead.
-              (_, lives') = mapAccumR (\live (_x', in_e) -> (live `S.union` inFreeVars annedTermFreeVars in_e, live)) (live `S.union` snd (stackFreeVars k (inFreeVars annedTermFreeVars (rn', e)))) xes'
+              makeLive live in_e = live `S.union` inFreeVars annedTermFreeVars in_e
+              (_, lives') = mapAccumR (\live (_x', in_e) -> (live `makeLive` in_e, live))
+                                      (M.fold (flip makeLive) (live `S.union` snd (stackFreeVars k (inFreeVars annedTermFreeVars (rn', e)))) h_losers) xes'
               
-              speculate_one (losers, deeds, Heap h ids) ((x', in_e), live')
-                = case reduce live' (losers, deeds, (Heap h ids, [], in_e)) of
+              speculate_one (losers, deeds, extra_live, h_losers, Heap h_winners ids) ((x', in_e), live')
+                = case reduce (live' `S.union` extra_live) (losers, deeds, (Heap h_winners ids, [], in_e)) of
                     -- NB: commenting in this line is useful if you want to test whether speculation is causing
                     -- a benchmark to be slow due to the cost of the speculation OR due to the extra info. propagation
                     --(rnf -> ()) | False -> undefined
-                    _ | not sPECULATE_ON_LOSERS, tg `IS.member` losers                 -> (losers,              deeds,  Heap (M.insert x' in_e  h)  ids)  -- Heuristic: don't speculate if it has failed before
-                    (losers', deeds', (Heap h' ids', [], in_e'@(_, annee -> Value _))) -> (losers',             deeds', Heap (M.insert x' in_e' h') ids') -- Speculation: if we can evaluate to a value "quickly" then use that value,
-                    _                                                                  -> (IS.insert tg losers, deeds,  Heap (M.insert x' in_e  h)  ids)  -- otherwise throw away the half-evaluated mess that we reach
+                    _ | not sPECULATE_ON_LOSERS, tg `IS.member` losers                 -> (losers,              deeds,  extra_live `makeLive` in_e, M.insert x' in_e  h_losers, Heap h_winners  ids)              -- Heuristic: don't speculate if it has failed before
+                    (losers', deeds', (Heap h' ids', [], in_e'@(_, annee -> Value _))) -> (losers',             deeds', extra_live,                 h_losers,                   Heap (M.insert x' in_e' h') ids') -- Speculation: if we can evaluate to a value "quickly" then use that value,
+                    _                                                                  -> (IS.insert tg losers, deeds,  extra_live `makeLive` in_e, M.insert x' in_e  h_losers, Heap h_winners  ids)              -- otherwise throw away the half-evaluated mess that we reach
                 where tg = annedTag (snd in_e)
