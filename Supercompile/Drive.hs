f1a51139f83e535b120ea828b589cc722731f9d0
Max Bolingbroke <batterseapower@hotmail.com>
2011-01-17 22:05:07 +0000

    Add comments about speculation control

--- Supercompile/Drive.hs
+++ Supercompile/Drive.hs
@@ -113,17 +113,26 @@ speculate reduce = snd . go (0 :: Int) (mkHistory wQO) emptyLosers
       where
         (deeds', (Heap h' ids', k, in_e)) = reduce (deeds, state)
         
-        -- It is *very important* that we prevent speculation from forcing heap-carried things that have proved
-        -- to be losers in the past. This prevents us discovering speculation failure of some head binding, and
-        -- then forcing several thunks that are each strict in it. This change was motivated by DigitsOfE2 -- it
-        -- speeds up supercompilation of that benchmark massively.
+        -- Note [Controlling speculation]
+        -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        --
+        -- Speculation gets out of hand easily. A motivating example is DigitsOfE2: without some way to control
+        -- speculation, it blows up, and I have not observed it to terminate.
+        --
+        -- One approach is using "losers". In this approach, we prevent speculation from forcing heap-carried things
+        -- that have proved to be losers in the past, as indicated by their tags being part of an accumulated losers set.
+        -- The losers set was threaded through "go", and hence generated anew for each call to "speculate".
+        --
+        -- An attractive approach (inspired by Simon, as usual!) is to just thread the *history* through go. This should have a
+        -- similiar effect, but prevents multiplication of mechanisms.
+        --
+        -- In my tests, the losers set approach ensured that DigitsOfE2 terminated after ~13s. Changing to the threaded
+        -- history approach, I did not observe termination :-(
         (h'_losers, h'_winners) | sPECULATE_ON_LOSERS = (M.empty, h')
                                 | otherwise           = M.partition (\hb -> maybe False (`IS.member` losers) (heapBindingTag_ hb)) h'
         
-        -- NB: It is important that we accumulate losers across "go" invocations in a state-monady kind of way, or DigitsOfE2 blows out
-        -- even more than normal (it still takes 22s with this change).
         -- TODO: I suspect we should accumulate Losers across the boundary of speculate as well
-        -- TODO: there is a difference between losers due to termination-halting and losers because we didn't have neough
+        -- TODO: there is a difference between losers due to termination-halting and losers because we didn't have enough
         -- information available to complete evaluation
         (deeds'', Heap h'_winners' ids'', losers') = M.foldrWithKey speculate_one (deeds', Heap h'_winners ids', losers) (h'_winners M.\\ h)
         speculate_one x' (Concrete in_e) (deeds, Heap h'_winners ids, losers)
